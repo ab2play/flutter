d3eecbe530e9a39fd307d97b1e2d930484bae83b
FlutLab.io <>
2022-08-22 02:36:37 +0000

    Autosave

--- lib/presentation/onBoarding/onBoarding.dart
+++ lib/presentation/onBoarding/onBoarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ksk/presentation/resources/color_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: ColorManager.darkprimary,
       body: Center(child: Text("Welcome to on boarding")),
     );
   }
